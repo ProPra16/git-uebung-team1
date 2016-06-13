63a382a1b393ed2a66e3e45700239d1c833fdea0
Florian-Dt <pic7-1@pic7.acs.uni-duesseldorf.de>
2016-06-13 09:57:11 +0200

    Schliessen-Button hinzugefügt

--- src/de/hhu/propra16/FiveButtons.java
+++ src/de/hhu/propra16/FiveButtons.java
@@ -1,6 +1,8 @@
 package de.hhu.propra16;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
--- src/de/hhu/propra16/FiveButtonsController.java
+++ src/de/hhu/propra16/FiveButtonsController.java
@@ -1,7 +1,20 @@
 package de.hhu.propra16;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 /**
  * Created by Tobias Witt on 06.06.16.
  */
 public class FiveButtonsController {
+	@FXML
+	private Button CloseButton;
+	
+    @FXML
+    protected void handleCloseButton(ActionEvent event) {
+    	Stage stage = (Stage) CloseButton.getScene().getWindow();
+        stage.close();
+    }
 }
